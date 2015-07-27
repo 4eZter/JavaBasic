9d152cbb5f887145bc9dc1ca189fbd5b6999c1b6
4eZter <cheztert@gmail.com>
2015-07-28 02:15:01 +0300

    homework 4

---
+++ sourseit/Trockiy/homework4/StackIml.java
@@ -0,0 +1,37 @@
+package org3.sourseit.Trockiy.homework.fourth;
+
+public class StackIml implements Stack {
+
+    Object[] objects = new Object[5];
+
+    @Override
+    public void push(Object o) throws StackException {
+        for (int i = 0; i < objects.length; i++){
+            //if empty
+            if(objects[i] == null){
+                if()
+            }
+        }
+
+    }
+
+    @Override
+    public Object pop() throws StackException {
+        for (int i = 0; i < objects.length; i++){
+
+        }
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < objects.length; i++){
+            objects[i] = null;
+        }
+    }
+
+    @Override
+    public void print() {
+        for (Object object : objects) object.toString();
+    }
+}
